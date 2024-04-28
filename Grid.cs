521dfa2b53a7cb6cbb858f31c92bd85e101443a7
Siddhesh Mahajan <siddheshmahajan99@gmail.com>
2024-04-29 00:22:37 +0530

    Add files via upload

---
+++ Grid.cs
@@ -0,0 +1,77 @@
+﻿namespace TicTacToe
+{
+    internal class Grid
+    {
+        public static char[] _arr;
+
+        public Grid(char[] arr)
+        {
+            _arr = arr;
+        }
+        public static void Print()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", _arr[1], _arr[2], _arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", _arr[4], _arr[5], _arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", _arr[7], _arr[8], _arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+
+        public static int Calculate()
+        {
+            // diag1
+            if ((_arr[1] == _arr[5]) && (_arr[5] == _arr[9]))
+            {
+                Console.Clear();
+                Declare(_arr[1]);
+                return 1;
+            }
+
+            // diag2
+            if ((_arr[3] == _arr[5]) && (_arr[5] == _arr[7]))
+            {
+                Console.Clear();
+                Declare(_arr[3]);
+                return 1;
+            }
+
+            // vert1
+            if ((_arr[1] == _arr[4]) && (_arr[4] == _arr[7]))
+            {
+                Console.Clear();
+                Declare(_arr[1]);
+                return 1;
+            }
+
+            // vert2
+            if ((_arr[2] == _arr[5]) && (_arr[5] == _arr[8]))
+            {
+                Console.Clear();
+                Declare(_arr[2]);
+                return 1;
+            }
+
+            // vert3
+            if ((_arr[3] == _arr[6]) && (_arr[6] == _arr[9]))
+            {
+                Console.Clear();
+                Declare(_arr[3]);
+                return 1;
+            }
+
+            Console.Clear();
+            return 0;
+        }
+
+        private static void Declare(char sign)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sign == 'X' ? "Player 1 Won!!" : "Player 2 Won!!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using static TicTacToe.Grid;
+
+namespace TicTacToe
+{
+    public class Program
+    {
+        static int _exit = 0;
+        private static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static void Main(string[] args)
+        {
+            Grid grid = new Grid(arr);
+            do
+            {
+                Print();
+                Console.WriteLine("Player 1 'X' input index:");
+                int xIndex = int.Parse(Console.ReadLine());
+                arr[xIndex] = 'X';
+
+                _exit = Calculate();
+                if (_exit == 1)
+                {
+                    break;
+                }
+                Print();
+
+                Console.WriteLine("Player 2 'O' input index:");
+                int oIndex = int.Parse(Console.ReadLine());
+                arr[oIndex] = 'O';
+                _exit = Calculate();
+            } while (_exit == 0);
+        }
+    }
+}
